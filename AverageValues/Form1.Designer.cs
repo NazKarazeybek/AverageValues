0662af97f9cab75ff453d356cf1be9fd065e29a6
NazKarazeybek <elifkara739@ed.amdsb.ca>
2024-03-05 14:44:05 -0500

    Proje dosyası ekle.

---
+++ AverageValues/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AverageValues
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.infoLabel2 = new System.Windows.Forms.Label();
+            this.valueInput = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkRed;
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, -3);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(606, 99);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Average Number Solver";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(123, 154);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(361, 43);
+            this.infoLabel.TabIndex = 1;
+            this.infoLabel.Text = "Enter A Number And Press Add";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel2
+            // 
+            this.infoLabel2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel2.ForeColor = System.Drawing.Color.White;
+            this.infoLabel2.Location = new System.Drawing.Point(89, 207);
+            this.infoLabel2.Name = "infoLabel2";
+            this.infoLabel2.Size = new System.Drawing.Size(420, 43);
+            this.infoLabel2.TabIndex = 2;
+            this.infoLabel2.Text = "(enter 0 and press add to calculate average)";
+            this.infoLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // valueInput
+            // 
+            this.valueInput.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueInput.Location = new System.Drawing.Point(197, 313);
+            this.valueInput.Name = "valueInput";
+            this.valueInput.Size = new System.Drawing.Size(100, 40);
+            this.valueInput.TabIndex = 3;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.Black;
+            this.addButton.ForeColor = System.Drawing.Color.White;
+            this.addButton.Location = new System.Drawing.Point(312, 313);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(88, 40);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Black;
+            this.resetButton.ForeColor = System.Drawing.Color.White;
+            this.resetButton.Location = new System.Drawing.Point(503, 493);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(88, 40);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.outputLabel.Location = new System.Drawing.Point(59, 392);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(484, 65);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(603, 545);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.valueInput);
+            this.Controls.Add(this.infoLabel2);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label infoLabel2;
+        private System.Windows.Forms.TextBox valueInput;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ AverageValues/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageValues
+{
+    public partial class Form1 : Form
+    {
+        private double sum = 0;
+        private int totalNumbers = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double number = Convert.ToDouble(valueInput.Text);
+                if (number == 0)
+                {
+                    double average = totalNumbers > 0 ? sum / totalNumbers : 0; 
+                    //? is calculates the average of all values entered. It checks (totalNumbers > 0). If yes, it calculates (sum / totalNumbers).
+                    outputLabel.Text = $"Average of all values entered: {average:F2}";
+                }
+                else
+                {
+                    sum += number;
+                    totalNumbers++;
+                    outputLabel.Text = $"Number added: {number}";
+                }
+            }
+            catch (FormatException)
+            {
+                outputLabel.Text = "Please enter a valid number.";
+            }
+
+            valueInput.Clear();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            totalNumbers = 0;
+            outputLabel.Text = "";
+            valueInput.Clear();
+        }
+    }
+}
